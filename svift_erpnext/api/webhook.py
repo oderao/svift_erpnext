a4e8ae3001421b3f7c6748b712dc6e659d8602ba
oderao <tripleo4u@gmail.com>
2023-11-15 12:24:04 +0100

    update status code and server messages

--- svift_erpnext/api/webhook.py
+++ svift_erpnext/api/webhook.py
@@ -23,8 +23,13 @@ def update_purchase_order(*args,**kwargs):
                 po_doc.flags.ignore_validate_update_after_submit=True
                 po_doc.flags.ignore_mandatory = True
                 po_doc.save()
-                frappe.local.response["status_code"] = 200
-                frappe.local.response["message"] = "Webhook Received"
+                frappe.local.response["http_status_code"] = 200
+                frappe.local.response['data'] = {"server":"Webhook Received and Processed"}
+                frappe.local.response['_server_messages'] = "Webhook Received and Processed"
+        else:
+            frappe.local.response["http_status_code"] = 404
+            frappe.local.response['data'] = {"server":"purchase order_does not exist"}
+            frappe.local.response["message"] = "Webhook Received"
     except Exception as e:
         frappe.local.response["status_code"] = 200
         frappe.local.response["message"] = "Webhook Received"
